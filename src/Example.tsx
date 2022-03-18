6b0396688f41276cd0d653a3bff248b2cb059447
mudy <mudy@mail.jj.cn>
2022-03-18 15:52:06 +0800

    fetch请求数据 使用useEffect，第二个参数传空数组

--- src/Example.tsx
+++ src/Example.tsx
@@ -2,10 +2,27 @@ import React, { useEffect, useState } from 'react'
 
 const Example = () => {
     const [count, setCount] = useState(0)
+    const [data, setData] = useState()
 
+    // useEffect(()=>{
+    //     document.title = `你点击了${count}次title`
+    // });
+
+    // useEffect(()=>{
+    //     document.title = `你点击了${count}次title`
+    // },[count]);
+
+    // count发生改变的时候 这个副作用函数才需要调用，所以这第二个参数[count] 的作用就是
+    // 如果传的是[] ，则只会在页面加载的时候调用一次 可以用来处理网络请求的数据
+    
     useEffect(()=>{
-        document.title = `你点击了${count}次title`
-    });
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response =>response.json())
+        .then(data=>{
+            console.log(data)
+            setData(data)
+        })
+    },[])
 
     return (
         <div>
